ea7689e892db87c71e344e2877b0078a861df17e
Jhamil <jturpoan@unsa.edu.pe>
2026-08-24 09:28:10 -0500

    fix(layout): remove CartDrawer component from Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,7 +25,6 @@ export default function Layout() {
       </main>
       <Footer />
       <WhatsAppButton />
-      <CartDrawer />
       <AnnouncementModal />
       <ToastContainer />
     </div>
